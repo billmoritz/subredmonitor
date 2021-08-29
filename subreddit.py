86134b4a951ac2decdea38d98760170bea060922
Bill Moritz <wmoritz@salesforce.com>
2021-08-29 14:56:45 -0400

    Update logging

--- subreddit.py
+++ subreddit.py
@@ -1,31 +1,45 @@
-#!/bin/env python3
+#!/usr/bin/env python3
 
+import logging
 import os
+import re
+import time
+
 import praw
 import pyprowl
-import re
 import redis
-import time
 import yaml
 
-# TODO: Better Logging
 # TODO: Hash body to catch edits
 # TODO: Get running on server
 
+LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
+DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+logging.basicConfig(level=logging.INFO, format=LOG_FORMAT, datefmt=DATE_FORMAT)
+
 try:
     CONFIG = yaml.full_load(open("config.yaml", 'r'))
-    print("Config file loaded!")
+    logging.info("Config file loaded!")
 except Exception as e:
-    print("Error loading config file: {}".format(e))
+    logging.critical("Error loading config file: {}".format(e))
+    exit()
+
+log_level = CONFIG['log_level'] if 'log_level' in CONFIG else 'INFO'
+logger = logging.getLogger()
+
+try:
+    logger.setLevel(log_level)
+except Exception as e:
+    logging.critical("Error setting log level: {}".format(e))
     exit()
 
 redis_cache = redis.Redis(host='redis', port=6379)
 
 try:
     redis_cache.ping()
-    print("Redis connection successfully verified!")
+    logging.debug("Redis connection successfully verified!")
 except Exception as e:
-    print("Error connecting to Redis: {}".format(e))
+    logging.critical("Error connecting to Redis: {}".format(e))
     exit()
 
 
@@ -45,18 +59,19 @@ prowl = pyprowl.Prowl(os.environ.get('PROWL_API_KEY'))
 
 try:
     prowl.verify_key()
-    print("Prowl API key successfully verified!")
+    logging.debug("Prowl API key successfully verified!")
 except Exception as e:
-    print("Error verifying Prowl API key: {}".format(e))
+    logging.critical("Error verifying Prowl API key: {}".format(e))
     exit()
 
 
 def notify_event(url, subreddit, desc):
     try:
-        prowl.notify(event='Hit', description=desc, priority=0, appName='subredmonitor', url=url)
-        print("Notification successfully sent to Prowl!")
+        prowl.notify(event='Hit', description=desc, priority=0,
+                     appName='subredmonitor', url=url)
+        logging.debug("Notification successfully sent to Prowl!")
     except Exception as e:
-        print("Error sending notification to Prowl: {}".format(e))
+        logging.error("Error sending notification to Prowl: {}".format(e))
 
 
 reddit = praw.Reddit(
@@ -67,33 +82,40 @@ reddit = praw.Reddit(
     username=os.environ.get('REDDIT_USERNAME'),
 )
 
-if type(CONFIG['subreddit']) == list :
+if type(CONFIG['subreddit']) == list:
     SUBREDDITS = '+'.join(CONFIG['subreddit'])
-elif type(CONFIG['subreddit']) == str :
+elif type(CONFIG['subreddit']) == str:
     SUBREDDITS = CONFIG['subreddit']
 else:
-    print("Config option 'subreddit' is invalid type: " + str(type(CONFIG['subreddit'])))
+    logging.critical(
+        "Config option 'subreddit' is invalid type: " + str(type(CONFIG['subreddit'])))
     exit()
 
-print('Monitoring {}'.format(SUBREDDITS))
+logging.info('Start monitoring: {}'.format(SUBREDDITS))
 
 for submission in reddit.subreddit(SUBREDDITS).stream.submissions():
     text_matched, title_matched = False, False
-    for title_match in CONFIG['title_matches']:
+    for title_match in CONFIG['title_match']:
         if submission.title.lower().find(title_match.lower()) != -1:
             title_matched = True
     if title_matched:
         if reddit.submission(submission.id).is_self:
             text = reddit.submission(submission.id).selftext
-            for text_match in CONFIG['text_matches']:
+            for text_match in CONFIG['text_match']:
                 if text.lower().find(text_match.lower()) != -1:
                     text_matched = True
             if text_matched:
                 hits = get_submission_hits(submission.id)
-                print('/u/{}: ({}) {}'.format(submission.author.name,
-                                              submission.id, submission.title))
+                logging.info('Matched submission from /u/{}: ({}) {}'.format(submission.author.name,
+                                                                           submission.id, submission.title))
                 if hits > 1:
-                    print(
+                    logging.info(
                         "Skipping notification because we've seen this {} times.".format(hits))
                 else:
                     notify_event(submission.url, SUBREDDITS, submission.title)
+            else:
+                logging.debug('Title matched but text did not /u/{}: ({}) {}'.format(submission.author.name,
+                                                                                     submission.id, submission.title))
+    else:
+        logging.debug('Title did not match /u/{}: ({}) {}'.format(submission.author.name,
+                                                                      submission.id, submission.title))
